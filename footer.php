c8874e1f490876ea32523c3e1b098c691f482c38
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-25 22:30:33 +0530

    add Header And Footer
    <?php get_header( ); ?>
    <h1> hi </hi>
    <?php get_footer( ); ?>
    for footer to know where footer end
    <?php wp_footer(); ?>
    for header
    <?php wp_head(); ?>

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
-Footer
+<div class="footer-clear"></div>
+<footer class="row no-max pad">
+  <?php wp_footer(); ?>            
+  <p><?php echo date('Y'); ?></p>
+</footer>
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php get_header( ); ?>
-<h1>index</h1>
+
 <?php get_footer( ); ?>
