a7d07ff9a9d8c4d527ffeec9202e9a10267bb856
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-27 20:55:00 +0530

    Nav Bug
    wp_enqueue_scripts Important To load Scripts

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 <div class="footer-clear"></div>
 <footer class="row no-max pad">
-  <?php wp_footer(); ?>            
+        
   <p><?php echo date('Y'); ?></p>
+ 
 </footer>
-
+<?php wp_footer(); ?>    
   </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 // connnect to css file 
 function wtb_theme_styles(){
-    wp_enqueue_style('founddation', get_template_directory_uri().'/css/foundation.css');
+    wp_enqueue_style('founddation_css', get_template_directory_uri().'/css/foundation.css');
     wp_enqueue_style('google_font','http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic');
    // wp_enqueue_style('normalize_css', get_template_directory_uri().'/css/normalize.css');
-    wp_enqueue_style('maincss', get_template_directory_uri().'/style.css');
+    wp_enqueue_style('main_css', get_template_directory_uri().'/style.css');
 
 }
 
@@ -13,11 +13,11 @@ add_action( 'wp_enqueue_scripts','wtb_theme_styles' );
 function wtb_theme_js(){
     
     wp_enqueue_script( 'mordernizer_js', get_template_directory_uri() . '/js/modernizr.js','','',false );
-    wp_enqueue_script( 'foundationjs', get_template_directory_uri() . '/js/foundation.js',array('jquery'),'',true );
-    wp_enqueue_script( 'appjs', get_template_directory_uri() . '/js/app.js',array('jquery','foundationjs'),'',true );
+    wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js',array('jquery'),'',true );
+    wp_enqueue_script( 'app_js', get_template_directory_uri() . '/js/app.js',array('jquery','foundation_js'),'',true );
     
 }
 
-add_action( 'wp_enqueue_script','wtb_theme_js')
+add_action( 'wp_enqueue_scripts','wtb_theme_js')
 
-?>
+?> 
