2c1ba212c51777ae118a6a0708112f874299c53e
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-12 11:02:23 +0530

    Split app using diffrent pages and add single show page

--- theme-two/index.php
+++ theme-two/index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes() ?>>
-<head>
-	<meta charset="<?php bloginfo('charset') ?>">
-	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title><?php bloginfo('name') ?></title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class() ?> >
-	<header>
-		<div class="container">
-			<h1>
-				<a href="<?php echo home_url('/') ?>">
-					<?php bloginfo('name'); ?>
-				</a>
-				<small><?php bloginfo('description')?></small>
-			</h1>
-			<div class="h_right">
-				<form method="GET" action="<?php esc_url(home_url('/')) ?>" >
-					<input type="text" placeholder="Search...">
-				</form>
-			</div>
-		</div>
-	</header>
-
-	<nav class="nav main-nav">
-		<div class="container">
-	<?php
-        $args = array(
-            'theme_location' => 'primary'
-        )
-    ?>
-	<?php wp_nav_menu($args) ?>
-		</div>
-	</nav>
+<?php get_header(); ?>
 
 	<div class="container content">
 		<div class="main block">
@@ -48,19 +12,19 @@
 					</a>	
 				 	On : <?php the_date(); ?>
 					Category: <?php
-					 $categories = get_the_category();
-					 $seprator = ',';
-					 $output = '';
-					 if($categories){
-						 foreach ($categories as $category) {
-							 $output = '<a href="'. get_category_link($category->term_id) .'">'. $category->cat_name .'</a>' . $seprator;
-						 }
-					 }
-					 echo trim($output,$seprator);
-					 ?>
+                     $categories = get_the_category();
+                     $seprator = ',';
+                     $output = '';
+                     if ($categories) {
+                         foreach ($categories as $category) {
+                             $output = '<a href="'. get_category_link($category->term_id) .'">'. $category->cat_name .'</a>' . $seprator;
+                         }
+                     }
+                     echo trim($output, $seprator);
+                     ?>
 
 				 </p>
-				<?php if(has_post_thumbnail()) : ?>
+				<?php if (has_post_thumbnail()) : ?>
 				<div class="post-thumbnail">
 					<?php the_post_thumbnail() ?>
 				</div>	
@@ -83,22 +47,4 @@
 			</div>
 		</div>
 	</div>
-
-	<footer class="main-footer">
-		<div class="container">
-			<div class="f_left">
-				<p>&copy; 2016 - Advanced WP Theme</p>
-			</div>
-			<div class="f_right">
-				<ul>
-					<li><a href="index.html">Home</a></li>
-					<li><a href="about.html">About</a></li>
-					<li><a href="#">Services</a></li>
-				</ul>
-			</div>
-
-		</div>
-	</footer>
-	<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
